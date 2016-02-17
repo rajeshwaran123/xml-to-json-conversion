83ff226ee7b1e9f530f5d9d25fdbdf3b7d1e4bd5
rajeshwaran123 <rajeshwaranv1990@gmail.com>
2016-02-17 19:37:44 +0530

    Create one.php

---
+++ one.php
@@ -0,0 +1,10 @@
+<html>
+   <body>
+      
+      <form action="one.php" method="POST" enctype="multipart/form-data">
+         
+         <input type="submit"/>
+      </form>
+      
+   </body>
+</html>
